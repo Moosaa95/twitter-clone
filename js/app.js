d8717c2e3a53f3e4bf1b26767605301ffe1c1b61
Musa Abdullahi <moosaabdullahi45@gmail.com>
2020-11-20 06:50:00 +0100

    add more detail to home page

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@ function qs(prop) {
     return document.querySelector(prop)
 }
 
+
+//getting the variables
+
 const signUp = qs('#signUpForm')
 const login = qs('#loginForm')
 const signUpScreen = qs('#signUp')
@@ -12,7 +15,11 @@ const loginScreen = qs('#login')
 const loginBtn = qs('#loginBtn')
 const home = qs('#home')
 const fluid = qs('.fluid-container')
-const msg = qs('.message')
+const userName = qs('#name').value
+const pass = qs('#password').value
+const logoutBtn = qs('#logout')
+const profileBtn = qs('#profileBtn')
+
 
 
 //open the sign up page
@@ -29,36 +36,48 @@ function hideSignUp() {
     signUpScreen.style.display = 'none'
 }
 
+function showLogout() {
+    logoutBtn.style.display = 'block'
+}
+
 //click event for sign up button
 signUpBtn.addEventListener('click', signupFunc)
     //click eevent for log in button
 loginBtn.addEventListener('click', loginFunc)
     //click event for close button
 closeBtn.addEventListener('click', hideSignUp)
+    //display the logout button
+profileBtn.addEventListener('click', showLogout)
+
+logoutBtn.addEventListener('click', function() {
+    fluid.style.display = 'none'
+    hideSignUp()
+    loginScreen.style.display = 'block'
+    home.style.display = 'none'
+
+
+
+})
 
 signUp.addEventListener('submit', function(e) {
     //prevent it from reloading automatically
     e.preventDefault()
         //create an object to hold data from the sign up page
+
     let formData = {
-            name: qs('#name').value,
-            password: qs('#password').value
+            name: userName,
+            password: pass
 
         }
         //converting the object to string 
     localStorage.setItem('formData', JSON.stringify(formData))
+    alert('success')
         //empty the input after submitting 
     name = qs('#name').value = ""
     password = qs('#password').value = ""
+
 })
 
-/* let name = qs('#name').value
-    let password = qs('#password').value
-    localStorage.setItem('name', JSON.stringify(name))
-    localStorage.setItem('password', JSON.stringify(password))
-        //localStorage.setItem('formData', JSON.stringify(formData))
-    console.log('user add');
-}) */
 
 
 login.addEventListener('submit', function(e) {
@@ -66,40 +85,19 @@ login.addEventListener('submit', function(e) {
     e.preventDefault()
 
 
-    name = qs('#name').value
-    password = qs('#password').value
+
     var user = JSON.parse(localStorage.getItem('formData'))
-    console.log(user.name);
-    if (user.name === name && password === user.password) {
+    if (userName === '' && pass === '') {
+        alert('fields should not be left empty')
+    } else if (user.name === userName && pass === user.password) {
         home.style.display = 'block'
         signUpScreen.style.display = 'none'
         loginScreen.style.display = 'none'
         fluid.style.display = 'none'
 
-        msg.style.display = 'block'
 
 
     } else {
-        console.log('nott');
+        console.log('check fills');
     }
 })
-
-/*  var user = JSON.parse(localStorage.getItem('name'))
- if (name == user) {
-     console.log('congrat');
- } */
-
-/* user = JSON.parse(localStorage.getItem('name'))
-pass = JSON.parse(localStorage.getItem('password')) */
-
-
-/*   if (user == name && pass == password) {
-      console.log('congrat');
-  } else {
-      name = JSON.parse(localStorage.getItem(name))
-  } */
-/* var entry = localStorage.getItem("formData")
-console.log(entry.name);
-if (name == entry.name && password == entry.password) {
-    console.log('contfsd');
-} */
